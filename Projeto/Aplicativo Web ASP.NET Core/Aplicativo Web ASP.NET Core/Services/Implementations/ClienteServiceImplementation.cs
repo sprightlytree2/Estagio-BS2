11e9bab7cf8e868628682468dd6deb0682db0253
SpringTree2 <luccasdaccosta@gmail.com>
2021-07-18 13:47:24 -0300

    Subindo prototipo dos metodos de retorno no serviço de implementaçao

---
+++ Projeto/Aplicativo Web ASP.NET Core/Aplicativo Web ASP.NET Core/Services/Implementations/ClienteServiceImplementation.cs
@@ -0,0 +1,21 @@
+﻿using Aplicativo_Web_ASP.NET_Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplicativo_Web_ASP.NET_Core.Services.Implementations
+{
+    public class ClienteServiceImplementation : IClienteService
+    {
+        public List<Cliente> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente FindByID(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
