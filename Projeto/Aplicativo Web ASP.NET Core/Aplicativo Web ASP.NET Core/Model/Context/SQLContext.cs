2a7d86c210ec3ca752a02f2939367b9d37b26f1d
SpringTree2 <luccasdaccosta@gmail.com>
2021-07-18 12:06:27 -0300

    Definindo conexao com o DB

---
+++ Projeto/Aplicativo Web ASP.NET Core/Aplicativo Web ASP.NET Core/Model/Context/SQLContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Aplicativo_Web_ASP.NET_Core.Model.Context
+{
+    public class SQLContext : DbContext
+    {
+        public SQLContext()
+        {
+
+        }
+        public SQLContext(DbContextOptions<SQLContext> options) : base(options) {}
+        
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
