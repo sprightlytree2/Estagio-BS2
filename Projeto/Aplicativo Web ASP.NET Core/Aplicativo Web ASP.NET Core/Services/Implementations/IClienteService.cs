1460ce2f8c79fdec056873d8ca687f66f57de05c
SpringTree2 <luccasdaccosta@gmail.com>
2021-07-18 12:26:49 -0300

    Criando serviço que retornara os clientes

---
+++ Projeto/Aplicativo Web ASP.NET Core/Aplicativo Web ASP.NET Core/Services/Implementations/IClienteService.cs
@@ -0,0 +1,12 @@
+﻿using Aplicativo_Web_ASP.NET_Core.Model;
+using System.Collections.Generic;
+
+namespace Aplicativo_Web_ASP.NET_Core.Services.Implementations
+{
+    public interface IClienteService
+    {
+        Cliente FindByID(long id);
+        List<Cliente> FindAll();
+
+    }
+}
