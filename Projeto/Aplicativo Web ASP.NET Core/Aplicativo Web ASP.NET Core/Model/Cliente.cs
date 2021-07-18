04c27b8cbe0f2f41cd6502620b6b050a9077df73
SpringTree2 <luccasdaccosta@gmail.com>
2021-07-18 11:19:18 -0300

    Criando a classe cliente

---
+++ Projeto/Aplicativo Web ASP.NET Core/Aplicativo Web ASP.NET Core/Model/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aplicativo_Web_ASP.NET_Core.Model
+{
+    [Table("CLIENTE")]
+    public class Cliente
+    {
+        [Column("ID")]
+        public int Id { get; set; }
+        [Column("NOME")]
+        public string Nome { get; set; }
+        [Column("DATA_CADASTRO")]
+        public DateTime Data_Cadastro { get; set; }
+    }
+}
